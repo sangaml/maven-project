967da4e65d5875e138ceda32eeb7ae315c0babcc
root <root@ip-172-31-34-72.us-east-2.compute.internal>
2018-05-20 08:04:03 +0000

    Changed greeter.java file

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,10 +12,12 @@ public class Greeter {
 
   }
 
-/**
- *
- */
+  /**
+   * @param 
+   * @return
+   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
+
